54daf7aa2554e9cc12eecb4ecbd5956b7711c199
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 21:50:30 +0700

    Создаем сервис для работы с конфигурацией

---
+++ Core/AppConfigProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace EducationalProject.Core
+{
+    public class AppConfigProvider : IAppConfigProvider
+    {
+        public string[] GetControllerTypes()
+        {
+            return AppConfig.MenuConfig.ControllerTypes;
+        }
+
+        public string GetControllersNamespace()
+        {
+            return AppConfig.MenuConfig.ControllersNamespace;
+        }
+
+        public string[] GetProgramNames()
+        {
+            return AppConfig.MenuConfig.ProgramNames;
+        }
+    }
+}
