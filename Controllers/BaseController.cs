819b46f4d97b1157edddd3bfb23a034b05de77b0
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 09:58:13 +0700

    Создать базовый контроллер

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,8 @@
+﻿namespace EducationalProject.Controllers
+{
+    public abstract class BaseController
+    {
+        public abstract string Name { get; }
+        public abstract void Run();
+    }
+}
