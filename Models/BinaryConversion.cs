bba90d49cdd2d3a8a7a11d3d6c503c40f9d787e9
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 10:40:38 +0700

    Добавлена модель Двоичного преобразования

---
+++ Models/BinaryConversion.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationalProject.Models
+{
+    public class BinaryConversion
+    {
+        public int DecimalValue { get; set; }
+        public string BinaryValue { get; set; }
+        public string FormattedBinary => $"{BinaryValue.Substring(0, 4)} {BinaryValue.Substring(4)}";
+        public bool IsValid => !string.IsNullOrEmpty(BinaryValue) && DecimalValue >= 0;
+    }
+}
