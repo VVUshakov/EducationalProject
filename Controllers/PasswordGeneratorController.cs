97be28a3c072c6ff8dc813b5362d78e0603dd13b
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 11:08:41 +0700

    Добавлен контроллер генератора паролей

---
+++ Controllers/PasswordGeneratorController.cs
@@ -0,0 +1,54 @@
+﻿using EducationalProject.Services;
+using EducationalProject.Views;
+
+namespace EducationalProject.Controllers
+{
+    public class PasswordGeneratorController : BaseController
+    {
+        private readonly PasswordGeneratorService _service;
+        private readonly PasswordGeneratorView _view;
+
+        public PasswordGeneratorController()
+        {
+            _service = new PasswordGeneratorService();
+            _view = new PasswordGeneratorView();
+        }
+
+        public override string Name => "Генератор паролей";
+
+        public override void Run()
+        {
+            ConsoleHelper.ClearAndShowHeader(Name);
+
+            // Показать описание программы
+            ConsoleHelper.ShowInfoBlock(
+                "О ГЕНЕРАТОРЕ ПАРОЛЕЙ",
+                new string[] {
+                    "Эта программа создает безопасные пароли.",
+                    "Вы можете настроить:",
+                    "• Длину пароля",
+                    "• Типы используемых символов",
+                    "• Уровень сложности",
+                    "",
+                    "Рекомендуется использовать пароли",
+                    "длиной не менее 12 символов."
+                }
+            );
+
+            // 1. Получить длину пароля
+            int length = _view.GetPasswordLength();
+
+            // 2. Получить настройки символов
+            var settings = _view.GetPasswordSettings();
+
+            // 3. Сгенерировать пароль
+            var result = _service.GeneratePassword(settings, length);
+
+            // 4. Показать результат
+            _view.ShowResult(result);
+
+            // 5. Ожидание нажатия клавиши
+            ConsoleHelper.WaitForAnyKey();
+        }
+    }
+}
