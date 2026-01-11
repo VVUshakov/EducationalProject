8fde2228820eea1a59ae1fad9a03bec74cbe1188
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 21:48:20 +0700

    Создаем реализацию фабрики контроллеров

---
+++ Factories/ControllerFactory.cs
@@ -0,0 +1,52 @@
+﻿using EducationalProject.Controllers;
+using EducationalProject.Core;
+using System.Reflection;
+
+namespace EducationalProject.Factories
+{
+    public class ControllerFactory : IControllerFactory
+    {
+        private readonly IAppConfigProvider _configProvider;
+
+        public ControllerFactory(IAppConfigProvider configProvider)
+        {
+            _configProvider = configProvider;
+        }
+
+        public List<BaseController> CreateControllers()
+        {
+            var controllers = new List<BaseController>();
+            string[] controllerTypes = _configProvider.GetControllerTypes();
+            string controllersNamespace = _configProvider.GetControllersNamespace();
+
+            foreach(string controllerTypeName in controllerTypes)
+            {
+                try
+                {
+                    string fullTypeName = $"{controllersNamespace}.{controllerTypeName}";
+                    Assembly currentAssembly = Assembly.GetExecutingAssembly();
+                    Type controllerType = currentAssembly.GetType(fullTypeName);
+
+                    if(controllerType != null)
+                    {
+                        BaseController controller = (BaseController)Activator.CreateInstance(controllerType);
+                        controllers.Add(controller);
+
+                        // Для отладки (нужно удалить)
+                        Console.WriteLine($"✓ Контроллер '{controllerTypeName}' успешно создан");
+                    }
+                    else
+                    {
+                        ConsoleHelper.ShowError($"Контроллер '{controllerTypeName}' не найден!");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    ConsoleHelper.ShowError($"Ошибка создания контроллера '{controllerTypeName}': {ex.Message}");
+                }
+            }
+
+            return controllers;
+        }
+    }
+}
