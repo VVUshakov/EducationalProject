6666fc99890cf8fea1fbc70aabcdbd6feee7c019
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 11:04:48 +0700

    Добавлена ​​модель Пароль-Результат

---
+++ Models/PasswordResult.cs
@@ -0,0 +1,12 @@
+﻿namespace EducationalProject.Models
+{
+    public class PasswordResult
+    {
+        public string Password { get; set; }
+        public int Length { get; set; }
+        public PasswordSettings Settings { get; set; }
+        public string Strength { get; set; }
+
+        public bool IsValid => !string.IsNullOrEmpty(Password) && Length > 0;
+    }
+}
