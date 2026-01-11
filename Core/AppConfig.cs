2657177f166742835597a5b5382448602fb6e077
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 10:02:22 +0700

    Создать единое место для настроек

--- Core/AppConfig.cs
+++ Core/AppConfig.cs
@@ -3,33 +3,48 @@
     public static class AppConfig
     {
         // Общие настройки
-        public const int MaxPasswordLength = 50;
-        public const int MinPasswordLength = 4;
-        public const string Currency = "руб.";
+        public const string PressAnyKeyMessage = "Нажмите любую клавишу для продолжения...";
+        public const string WelcomeMessage = "Добро пожаловать в учебный проект!";
+        public const string GoodbyeMessage = "Спасибо за использование программы!";
+        public const string ErrorTitle = "ОШИБКА";
+        public const string SuccessTitle = "УСПЕХ";
+        public const string DescriptionTitle = "ОПИСАНИЕ";
 
-        // Сообщения
-        public static class Messages
+        // Консольное оформление
+        public const int FrameWidth = 35;
+        public const char FrameTopLeft = '╔';
+        public const char FrameTopRight = '╗';
+        public const char FrameBottomLeft = '╚';
+        public const char FrameBottomRight = '╝';
+        public const char FrameVertical = '║';
+        public const char FrameHorizontal = '═';
+
+        // Настройки калькулятора
+        public static class Calculator
         {
-            public const string PressAnyKey = "Нажмите любую клавишу для продолжения...";
-            public const string Welcome = "Добро пожаловать в учебный проект!";
-            public const string Goodbye = "Спасибо за использование программы!";
+            public const string Name = "Калькулятор";
+            public static readonly string[] OperationNames = { "Сложение", "Вычитание", "Умножение", "Деление", "Остаток от деления" };
+            public static readonly char[] OperationSymbols = { '+', '-', '*', '/', '%' };
         }
 
-        // Диапазоны значений
-        public static class Ranges
+        // Настройки пароля
+        public static class Password
         {
-            public const int BinaryMin = 0;
-            public const int BinaryMax = 255;
-            public const int BinaryBits = 8;
-        }
-
-        // Символы
-        public static class Characters
-        {
+            public const int MinLength = 4;
+            public const int MaxLength = 50;
+            public const int DefaultLength = 12;
             public const string LowerCase = "abcdefghijklmnopqrstuvwxyz";
             public const string UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             public const string Digits = "0123456789";
-            public const string Special = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+            public const string SpecialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+        }
+
+        // Настройки двоичного преобразователя
+        public static class Binary
+        {
+            public const int MinValue = 0;
+            public const int MaxValue = 255;
+            public const int BitsCount = 8;
         }
     }
 }
