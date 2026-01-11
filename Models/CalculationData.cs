53933093d118a4dab8d4b4b7adcf6810402afc36
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 10:14:03 +0700

    Создание модели калькулятора

---
+++ Models/CalculationData.cs
@@ -0,0 +1,15 @@
+﻿namespace EducationalProject.Models
+{
+    public class CalculationData
+    {
+        public double FirstNumber { get; set; }
+        public double SecondNumber { get; set; }
+        public char Operation { get; set; }
+        public double Result { get; set; }
+        public double? Remainder { get; set; }
+        public bool IsValid => !double.IsNaN(Result);
+
+        // Для удобства - описание вычисление
+        public string Description => $"{FirstNumber} {Operation} {SecondNumber}";
+    }
+}
