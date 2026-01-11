15a83e631ba5d0e9da4fd4e0f19667cd0bfa7452
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 11:16:17 +0700

    Добавлен контроллер AssignmentDemoController

---
+++ Controllers/AssignmentDemoController.cs
@@ -0,0 +1,68 @@
+﻿using EducationalProject.Services;
+using EducationalProject.Views;
+
+namespace EducationalProject.Controllers
+{
+    public class AssignmentDemoController : BaseController
+    {
+        private readonly AssignmentDemoService _service;
+        private readonly AssignmentDemoView _view;
+
+        public AssignmentDemoController()
+        {
+            _service = new AssignmentDemoService();
+            _view = new AssignmentDemoView();
+        }
+
+        public override string Name => "Демо: Операции присваивания";
+
+        public override void Run()
+        {
+            ConsoleHelper.ClearAndShowHeader(Name);
+
+            // Показать описание программы
+            ConsoleHelper.ShowInfoBlock(
+                "О ПРОГРАММЕ",
+                new string[] {
+                    "Эта программа демонстрирует операции присваивания в C#.",
+                    "Вы узнаете как использовать:",
+                    "• Простые операции: +=, -=, *=, /=, %=",
+                    "• Побитовые операции: <<=, >>=",
+                    "• Комбинированные операции",
+                    "• Практические примеры из жизни"
+                }
+            );
+
+            // 1. Получить выбор демонстрации
+            int choice = _view.GetDemoChoice();
+
+            // 2. Выполнить выбранную демонстрацию
+            switch(choice)
+            {
+                case 1:
+                    var basicData = _service.GetBasicOperations();
+                    _view.ShowBasicOperations(basicData);
+                    break;
+
+                case 2:
+                    var combinedData = _service.GetCombinedOperations();
+                    _view.ShowCombinedOperations(combinedData);
+                    break;
+
+                case 3:
+                    var practicalExample = _service.GetPracticalExample();
+                    _view.ShowPracticalExample(practicalExample);
+                    break;
+            }
+
+            // 3. Показать дополнительную информацию
+            _view.ShowAssignmentInfo();
+
+            // 4. Показать таблицу примеров
+            _view.ShowExamplesTable();
+
+            // 5. Ожидание нажатия клавиши
+            ConsoleHelper.WaitForAnyKey();
+        }
+    }
+}
