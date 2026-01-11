fb8ebb6b2de0866456c5842198ed547ad7c57c58
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 10:54:01 +0700

    Добавлена ​​модель Результата кодирования

---
+++ Models/EncodingResult.cs
@@ -0,0 +1,12 @@
+﻿namespace EducationalProject.Models
+{
+    public class EncodingResult
+    {
+        public string OriginalName { get; set; }
+        public string EncodedName { get; set; }
+        public string MethodName { get; set; }
+        public int MethodChoice { get; set; }
+
+        public bool IsValid => !string.IsNullOrEmpty(EncodedName);
+    }
+}
