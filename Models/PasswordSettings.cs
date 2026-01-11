3b0db8c73525b01d8e4746e66d707a2541afbb04
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-11 11:03:31 +0700

    Добавлена модель Настроек пароля

---
+++ Models/PasswordSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace EducationalProject.Models
+{
+    public class PasswordSettings
+    {
+        public bool UseLower { get; set; } = true;
+        public bool UseUpper { get; set; } = true;
+        public bool UseDigits { get; set; } = true;
+        public bool UseSpecial { get; set; } = true;
+
+        public bool HasAnySetting => UseLower || UseUpper || UseDigits || UseSpecial;
+        public int SettingsCount => (UseLower ? 1 : 0) + (UseUpper ? 1 : 0) +
+                                  (UseDigits ? 1 : 0) + (UseSpecial ? 1 : 0);
+    }
+}
