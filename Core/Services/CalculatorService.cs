15096b95308d9e4d80e98097d384c789d2b3279b
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-31 22:08:51 +0700

    Создать простой сервис Калькулятора

---
+++ Core/Services/CalculatorService.cs
@@ -0,0 +1,28 @@
+﻿namespace EducationalProject.Core.Services
+{
+    public class CalculatorService
+    {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+
+        public double Divide(double a, double b)
+        {
+            if(b == 0)
+                throw new DivideByZeroException("Деление на ноль невозможно!");
+            return a / b;
+        }
+
+        public double Power(double number, int power)
+        {
+            return Math.Pow(number, power);
+        }
+
+        public double SquareRoot(double number)
+        {
+            if(number < 0)
+                throw new ArgumentException("Корень из отрицательного числа!");
+            return Math.Sqrt(number);
+        }
+    }
+}
