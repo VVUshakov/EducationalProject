e48fa927d5a5a6e561795480ffb3b7bd9ce34763
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-31 21:49:55 +0700

    Создать модель пункта меню

---
+++ Core/Models/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿namespace EducationalProject.Core.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Action Action { get; set; }
+
+        public MenuItem(int id, string title, Action action)
+        {
+            Id = id;
+            Title = title;
+            Action = action;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@
             // Настройка приложения
             var view = new ConsoleView();
             var programs = new List<BaseProgram>
-        {
-            new CalculatorProgram(),
-            new NameEncoderProgram()
-            // Добавить новую программу можно просто создав новый экземпляр
-        };
+            {
+                new CalculatorProgram(),
+                new NameEncoderProgram()
+                // Добавить новую программу можно просто создав новый экземпляр
+            };
 
             var controller = new MainController(view, programs);
 
