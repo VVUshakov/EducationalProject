0eaaafbc92d694cd3f964299b277282642cc8fff
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-31 21:58:07 +0700

    Создать абстрактный класс для разных программ

---
+++ Programs/BaseProgram.cs
@@ -0,0 +1,28 @@
+﻿using EducationalProject.Core.Interfaces;
+using EducationalProject.Core.Views;
+
+namespace EducationalProject.Programs
+{
+    public abstract class BaseProgram : IProgram
+    {
+        protected readonly IView _view;
+
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+
+        protected BaseProgram(IView view = null)
+        {
+            _view = view ?? new ConsoleView();
+        }
+
+        public abstract void Execute();
+
+        protected void ShowHeader()
+        {
+            _view.Clear();
+            _view.ShowMessage($"=== {Name.ToUpper()} ===");
+            _view.ShowMessage(Description);
+            _view.ShowMessage("");
+        }
+    }
+}
