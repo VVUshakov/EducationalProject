40eb862b815f89c10280146ecf012071e13ce33c
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-31 21:50:39 +0700

    Создать модель Меню

---
+++ Core/Models/Menu.cs
@@ -0,0 +1,22 @@
+﻿namespace EducationalProject.Core.Models
+{
+    public class Menu
+    {
+        public List<MenuItem> Items { get; private set; }
+
+        public Menu()
+        {
+            Items = new List<MenuItem>();
+        }
+
+        public void AddItem(MenuItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(int id)
+        {
+            Items.RemoveAll(item => item.Id == id);
+        }
+    }
+}
