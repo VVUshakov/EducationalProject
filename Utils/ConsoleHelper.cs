9c4e6abb24db5b4232babc474e8c229e93333563
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-31 22:05:38 +0700

    Создать утилиту с вспомогательными методами

---
+++ Utils/ConsoleHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace EducationalProject.Utils
+{
+    public static class ConsoleHelper
+    {
+        public static void PrintColored(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+
+        public static void PrintSuccess(string message)
+        {
+            PrintColored(message, ConsoleColor.Green);
+        }
+
+        public static void PrintWarning(string message)
+        {
+            PrintColored(message, ConsoleColor.Yellow);
+        }
+
+        public static void PrintError(string message)
+        {
+            PrintColored(message, ConsoleColor.Red);
+        }
+
+        public static void PrintHeader(string title)
+        {
+            Console.WriteLine(new string('=', title.Length + 4));
+            Console.WriteLine($"  {title}");
+            Console.WriteLine(new string('=', title.Length + 4));
+        }
+    }
+}
