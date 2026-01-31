ec8aab575d045697432f8a917c911ffea92b8be8
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-31 22:15:54 +0700

    Инструкция по расширению (добавление новой программы)

---
+++ Programs/MyNewProgram.cs
@@ -0,0 +1,19 @@
+﻿using EducationalProject.Core.Views;
+
+namespace EducationalProject.Programs
+{
+    public class MyNewProgram : BaseProgram
+    {
+        public override string Name => "Моя новая программа";
+        public override string Description => "Описание новой программы";
+
+        public MyNewProgram() : base(new ConsoleView()) { }
+
+        public override void Execute()
+        {
+            ShowHeader();
+            // Логика вашей программы здесь
+            _view.ShowMessage("Привет от новой программы!");
+        }
+    }
+}
