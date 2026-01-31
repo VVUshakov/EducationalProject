ec6253607cd06e7fee42c893dee61ca54f6ea4d0
Владимир Ушаков <svayznoy@inbox.ru>
2026-01-31 21:54:02 +0700

    Создать базовый Контроллер

---
+++ Core/Controllers/ProgramController.cs
@@ -0,0 +1,16 @@
+﻿using EducationalProject.Core.Interfaces;
+
+namespace EducationalProject.Core.Controllers
+{
+    public abstract class ProgramController : IController
+    {
+        protected readonly IView _view;
+
+        protected ProgramController(IView view)
+        {
+            _view = view;
+        }
+
+        public abstract void Run();
+    }
+}
